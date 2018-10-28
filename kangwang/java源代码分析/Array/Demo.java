acd446c49833bab64e1bd8c06f4d468aa7f23689
wangwang <2818815189@qq.com>
2018-10-28 12:38:22 +0800

    更新html和源码部分

--- kangwang/java源代码分析/Array/Demo.java
+++ kangwang/java源代码分析/Array/Demo.java
@@ -1,21 +1,28 @@
+import java.util.ArrayDeque;
+import java.util.Arrays;
+
 class Demo{
     public static void main(String[] args) {
-        List list=new ArrayList();
-        
-        list.add("aa");
-
+        Arrays a =Arrays.asList("","");
+        ArrayDeque ar = new ArrayDeque<>();
     }
+    /**
+     * 
+     * @param arrayLength
+     * @param fromIndex
+     * @param toIndex
+     */
+    private static void rangeCheck(int arrayLength, int fromIndex, int toIndex) {
+        if (fromIndex > toIndex) {
+            throw new IllegalArgumentException(
+                    "fromIndex(" + fromIndex + ") > toIndex(" + toIndex + ")");
+        }
+        if (fromIndex < 0) {
+            throw new ArrayIndexOutOfBoundsException(fromIndex);
+        }
+        if (toIndex > arrayLength) {
+            throw new ArrayIndexOutOfBoundsException(toIndex);
+        }
+    }
+    
 }
-、】
-
-
-
-XXX.loder("bb.tets")
-
-
-XXX
-
-aa.A a=new aa.A();
-
-
-bb.A b 
